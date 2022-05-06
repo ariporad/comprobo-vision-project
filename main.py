dd0ee9f06baf99e22ab29d8a6f2fa25bc0b80b79
Ari Porad <ari@ariporad.com>
2022-05-06 16:22:28 -0400

    refactor: complete refactor

--- main.py
+++ main.py
@@ -224,59 +224,52 @@ class VOdom:
 	## Bundle Adjustment
 	# Based on this tutorial: https://scipy-cookbook.readthedocs.io/items/bundle_adjustment.html, but mostly re-written (except as otherwise marked).
 
-	
-	# Relevant data, in their implementation:
-	# - camera_params with shape (n_cameras, 9) contains initial estimates of parameters for all cameras. First 3 components in each row form a rotation vector (https://en.wikipedia.org/wiki/Rodrigues%27_rotation_formula), next 3 components form a translation vector, then a focal distance and two distortion parameters.
-	# - points_3d with shape (n_points, 3) contains initial estimates of point coordinates in the world frame.
-	# - camera_ind with shape (n_observations,) contains indices of cameras (from 0 to n_cameras - 1) involved in each observation.
-	# - point_ind with shape (n_observations,) contatins indices of points (from 0 to n_points - 1) involved in each observation.
-	# - points_2d with shape (n_observations, 2) contains measured 2-D coordinates of points projected on images in each observations.
+	FRAME_P_SIZE = 12
+	POINT3D_SIZE = 3
+
+	def _residual(self, state, num_frames, num_points3d, frame_idxs, point3d_idxs, points2d):
+		frame_state_len = (num_frames * self.FRAME_P_SIZE)
+		frame_Ps = state[:frame_state_len].reshape((num_frames, 3, 4))
+		points3d = state[frame_state_len:].reshape((num_points3d, self.POINT3D_SIZE))
+
+		P_by_frame = frame_Ps[frame_idxs, :, :]
+		point3d_by_frame = points3d[point3d_idxs]
+		point2d_by_frame = points2d
+		# Add a 4th column to make homogenous multiplication work
+		point3d_by_frame = np.hstack(
+			(point3d_by_frame, [[1]] * point3d_by_frame.shape[0]))
+
+		# Credit to this SO answer for making the math work so nicely: https://stackoverflow.com/a/66971088
+		reprojected = np.einsum('kij,kj->ki', (self.K @ P_by_frame), point3d_by_frame)
+		point2d_reproj = reprojected[:, :2] / reprojected[:, 2, None]
+
+		return (point2d_reproj - point2d_by_frame).ravel()
 
 	@timed
 	def bundle_adjustment(self, Ps, draw=False):
+		num_frames = len(Ps)
+		num_observations = self.point_cloud.num_observations
+		num_points3d = self.point_cloud.num_points3d
+
 		frame_Ps = np.array(list(Ps))
 		points3d = np.array(list(self.point_cloud.points3d))
-		frame_idx = np.zeros((self.point_cloud.num_observations,), dtype=int)
-		point3d_idx = np.zeros((self.point_cloud.num_observations,), dtype=int)
-		points2d = np.zeros((self.point_cloud.num_observations, 2))
-
-		n_frames = frame_Ps.shape[0]
-		n_observations = points3d.shape[0]
+		frame_idxs = np.zeros((num_observations,), dtype=int)
+		point3d_idxs = np.zeros((num_observations,), dtype=int)
+		points2d = np.zeros((num_observations, 2))
 
 		observation_id = 0
-		for point3d_id, point3d in enumerate(points3d):
+		for point3d_id, point3d in enumerate(self.point_cloud.points3d):
 			for frame_id, point2d in self.point_cloud.lookup3d(point3d):
-				frame_idx[observation_id] = frame_id
-				point3d_idx[observation_id] = point3d_id
+				frame_idxs[observation_id] = frame_id
+				point3d_idxs[observation_id] = point3d_id
 				points2d[observation_id, :] = point2d
 				observation_id += 1
 
 		state0 = np.hstack((frame_Ps.ravel(), points3d.ravel()))
-
-		FRAME_P_SIZE = 12
-		POINT3D_SIZE = 3
-
-		def _residual(state, K, num_Ps, num_points3d, frame_idx, point3d_idx, points2d):
-			frame_state_len = (num_Ps * FRAME_P_SIZE)
-			frame_Ps = state[:frame_state_len].reshape((num_Ps, 3, 4))
-			points3d = state[frame_state_len:].reshape((num_points3d, POINT3D_SIZE))
-
-			P_by_frame = frame_Ps[frame_idx, :, :]
-			point3d_by_frame = points3d[point3d_idx]
-			point2d_by_frame = points2d
-			# Add a 4th column to make homogenous multiplication work
-			point3d_by_frame = np.hstack(
-				(point3d_by_frame, [[1]] * point3d_by_frame.shape[0]))
-
-			# Credit to this SO answer for making the math work so nicely: https://stackoverflow.com/a/66971088
-			reprojected = np.einsum('kij,kj->ki', (K @ P_by_frame), point3d_by_frame)
-			point2d_reproj = reprojected[:, :2] / reprojected[:, 2, None]
-
-			return (point2d_reproj - point2d_by_frame).ravel()
-
+		
 		if draw:
-			residual0 = _residual(
-				state0, self.K, frame_Ps.shape[0], points3d.shape[0], frame_idx, point3d_idx, points2d)
+			residual0 = self._residual(
+				state0, num_frames, num_points3d, frame_idxs, point3d_idxs, points2d)
 			plt.figure()
 			plt.plot(residual0)
 			plt.title('Residuals before bundle adjustment')
@@ -284,21 +277,22 @@ class VOdom:
 
 		### Generate Sparsity Matrix ###
 		# This logic is completely taken from the SciPy tutorial
-		resid_len = frame_idx.size * 2
-		state_len = (n_frames * FRAME_P_SIZE) + (n_observations * POINT3D_SIZE)
-		A = lil_matrix((resid_len, state_len), dtype=int)
-		i = np.arange(frame_idx.size)
-		for offset in range(FRAME_P_SIZE):
-			A[2 * i, (frame_idx * FRAME_P_SIZE) + offset] = 1
-			A[2 * i + 1, (frame_idx * FRAME_P_SIZE) + offset] = 1
-		for offset in range(POINT3D_SIZE):
-			A[2 * i, (n_frames * FRAME_P_SIZE) +
-			  (point3d_idx * POINT3D_SIZE) + offset] = 1
-			A[2 * i + 1, (n_frames * FRAME_P_SIZE) +
-			  (point3d_idx * POINT3D_SIZE) + offset] = 1
+		resid_len = num_observations * 2
+		state_len = (num_frames * self.FRAME_P_SIZE) + (num_observations * self.POINT3D_SIZE)
+		sparsity = lil_matrix((resid_len, state_len), dtype=int)
+		# I can't figure out why we need the 2*i nonsense, but we do
+		i = np.arange(frame_idxs.size)
+		for offset in range(self.FRAME_P_SIZE):
+			sparsity[2 * i, (frame_idxs * self.FRAME_P_SIZE) + offset] = 1
+			sparsity[2 * i + 1, (frame_idxs * self.FRAME_P_SIZE) + offset] = 1
+		for offset in range(self.POINT3D_SIZE):
+			sparsity[2 * i, (num_frames * self.FRAME_P_SIZE) +
+			  (point3d_idxs * self.POINT3D_SIZE) + offset] = 1
+			sparsity[2 * i + 1, (num_frames * self.FRAME_P_SIZE) +
+			  (point3d_idxs * self.POINT3D_SIZE) + offset] = 1
 
-		res = least_squares(_residual, state0, verbose=2 if draw else 0, x_scale='jac',
-							jac_sparsity=A, method='trf', args=(self.K, n_frames, n_observations, frame_idx, point3d_idx, points2d))
+		res = least_squares(self._residual, state0, verbose=2 if draw else 0, x_scale='jac',
+							jac_sparsity=sparsity, method='trf', args=(num_frames, num_points3d, frame_idxs, point3d_idxs, points2d))
 
 		if draw:
 			plt.figure()
@@ -306,30 +300,25 @@ class VOdom:
 			plt.title('Residuals after bundle adjustment')
 			plt.show()
 
-		Ps_flat = res.x[:(FRAME_P_SIZE * n_frames)]
-		points3d_flat = res.x[(FRAME_P_SIZE * n_frames):]
+		Ps_flat = res.x[:(self.FRAME_P_SIZE * num_frames)]
+		points3d_flat = res.x[(self.FRAME_P_SIZE * num_frames):]
 
-		Ps = Ps_flat.reshape((n_frames, 3, 4))
+		Ps = Ps_flat.reshape((num_frames, 3, 4))
 		points3d = points3d_flat.reshape((self.point_cloud.num_points3d, 3))
 
 		return Ps
 
 
-	# poses = bundle_adjustment(kitti.calib.K_cam0, Ps, point_cloud, True)
-
-
-	# ## Scale Factor (Attempt)
+	## Scale Factor
 	#
 	# The idea: for some scale factor $C$, the following must be true:
+	#     P_calc * C = P_true
+	# We know P_calc and P_true so solve for C (an approximate solution, not an exact one).
 	#
-	# $$PC = P_{truth}$$
-	#
-	# We know $P$ and $P_{truth}$, so solve for $C$ (an approximate solution, not an exact one).
-	#
-	# But: what shape should $C$ have? Not sure, trying 4x4, we'll see if that works.
+	# But: what shape should C have? Not sure, trying 4x4, we'll see if that works.
 
 	@timed
-	def scale_factor(self, poses):
+	def calculate_scale_factor(self, poses):
 		P_calculated = np.array(poses).reshape((len(poses), -1))
 		P_truth = np.array(self.known_poses).reshape((len(poses), -1))
 
@@ -341,16 +330,13 @@ class VOdom:
 		corrected_poses = list(P_corrected)
 		return corrected_poses
 
-
-	# corrected_poses = scale_factor(poses, kitti)
-
-	# ## Error Calculation
+	## Error Calculation
 	#
 	# Attempts to quantify the accuracy of the algorithm. Currently, it does so per-axis using the following algorithm:
 	#
-	# $$\text{Error} = \text{mean}\left(\frac{P_{\text{ground truth, t}} - P_{\text{calculated, t}}}{P_{\text{ground truth, t}}}\right)$$
+	# Error = mean((P_truth - P_calc) / P_truth)$$
 	#
-	# Where $P$ is the robot's position (note that, currently, this doesn't consider orientation), and $t$ is the current timestep.
+	# Where P is the robot's position (note that, currently, this doesn't consider orientation)
 
 	def calculate_error(self, actual_poses, calculated_poses):
 		loc_odom = np.array(calculated_poses) @ np.transpose([0, 0, 0, 1])
@@ -361,29 +347,13 @@ class VOdom:
 
 		return x_err, y_err, z_err
 
-
-	# x_err, y_err, z_err = calculate_error(
-	# 	corrected_poses, kitti.poses[:len(corrected_poses)])
-
-	# %% [markdown]
-	# **For Drive 00:**
-	# 1.1% on the Z-axis is incredible! I'm really happy with that. Ditto for the Y-axis!
-	#
-	# I'm skeptical of their data for the X-axis... looking at the video, there's some real shake. Very confused about this.
-	#
-	# **For Drive 01 (every other frame):**
-	# 8% all around is still great! (And seems much more plausible). Really happy with this
-	#
-	# **For Drive 01 (every frame):**
-	# 7-9% on Y and Z is still great! Wtf is going on with X.
-
 	def run(self, draw: bool = False):
 		P_original = self.determine_projections()
 		print("Projections done!")
 		P_adjusted = self.bundle_adjustment(P_original, draw=draw)
 		print("Adjustment done")
 		raw_poses = self.projections_to_poses(P_adjusted)
-		scaled_poses = self.scale_factor(raw_poses)
+		scaled_poses = self.calculate_scale_factor(raw_poses)
 		x_err, y_err, z_err = self.calculate_error(self.known_poses, scaled_poses)
 		print(f"Mean error: X: {x_err:2.1f}%, Y: {y_err:2.1f}%, Z: {z_err:2.1f}%")
 
@@ -394,8 +364,7 @@ class VOdom:
 			ax.set_ylim(0, 1)
 			ax.set_zlim(0, 1)
 
-			# plot_trajectory(ax, raw_poses, label='Unscaled', line_color='b', show_arrows=False)
-			plot_trajectory(ax, scaled_poses, label='Scaled', line_color='r', show_arrows=False)
+			plot_trajectory(ax, scaled_poses, label='Calculated', line_color='r', show_arrows=False)
 			plot_trajectory(ax, self.known_poses, label='Truth', line_color='g--', show_arrows=False)
 
 			ax.set_xlabel('X')
